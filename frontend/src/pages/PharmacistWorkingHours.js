46fa55338661e4385186ccf08bf0e0013191eee4
jovan-jelicki <jovanzte@gmail.com>
2021-01-15 23:10:26 +0100

    Modal dialog for working hours

--- frontend/src/pages/PharmacistWorkingHours.js
+++ frontend/src/pages/PharmacistWorkingHours.js
@@ -2,13 +2,16 @@ import React from "react";
 import { Calendar, momentLocalizer , Views} from 'react-big-calendar'
 import moment from 'moment'
 import 'react-big-calendar/lib/css/react-big-calendar.css';
+import {Button, Modal} from "react-bootstrap";
 
 
 export default class PharmacistWorkingHours extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            myEvents : []
+            myEvents : [],
+            showModal : false,
+            event : ""
         }
     }
 
@@ -25,9 +28,10 @@ export default class PharmacistWorkingHours extends React.Component {
             },
             {
                 allDay: false,
-                start: new Date(2021, 0, 20, 12 , 30 , 0 ),
-                end: new Date(2021, 0, 20, 13, 30, 0),
+                start: new Date(2021, 1, 20, 12 , 30 , 0 ),
+                end: new Date(2021, 1, 20, 13, 30, 0),
                 title: 'All Day Event',
+                desc: 'Pre-meeting meeting, to prepare for the meeting'
             },
         ];
         const MyCalendar = (
@@ -40,6 +44,7 @@ export default class PharmacistWorkingHours extends React.Component {
                     step={20}
                     timeslots={2}
                     showMultiDayTimes={true}
+                    onDoubleClickEvent={event => this.handleModal(event)}
                     components={{
                         timeSlotWrapper: this.ColoredDateCellWrapper,
                     }}
@@ -52,8 +57,29 @@ export default class PharmacistWorkingHours extends React.Component {
             <div height="200px">
                 <br/>
                 {MyCalendar}
+                <Modal backdrop="static" show={this.state.showModal} onHide={this.handleModal}>
+                    <Modal.Header>
+                        <Modal.Title>{this.state.event.title}</Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body>
+                        {this.state.event.desc} <br/>
+                        Date-time: {moment(this.state.event.start).format('DD.MM.YYYY hh:mm')} -- {moment(this.state.event.end).format('DD.MM.YYYY hh:mm')}
+                    </Modal.Body>
+                    <Modal.Footer>
+                        <Button variant="secondary" onClick={this.handleModal}>
+                            Close
+                        </Button>
+                    </Modal.Footer>
+                </Modal>
             </div>
         );
+    }
+
+    handleModal = (event) => {
+        this.setState({
+            showModal : !this.state.showModal,
+            event : event
+        });
     }
 
     ColoredDateCellWrapper = ({ children }) =>
