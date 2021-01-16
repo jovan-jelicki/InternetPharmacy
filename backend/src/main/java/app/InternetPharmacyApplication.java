3cbda1c5fcf1be550435755c287449fac2b42e8f
Ilija Brdar <ilija_brdar@yahoo.com>
2021-01-16 14:30:17 +0100

    cors: all origins allowed

--- backend/src/main/java/app/InternetPharmacyApplication.java
+++ backend/src/main/java/app/InternetPharmacyApplication.java
@@ -3,6 +3,9 @@ package app;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 
 @SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
@@ -12,4 +15,13 @@ public class InternetPharmacyApplication {
 		SpringApplication.run(InternetPharmacyApplication.class, args);
 	}
 
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("*");
+			}
+		};
+	}
 }
