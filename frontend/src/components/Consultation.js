48a8d8e854306f861e0d78199cf501095b1c96a2
jovan-jelicki <jovanzte@gmail.com>
2021-01-17 23:51:21 +0100

    medication select option

--- frontend/src/components/Consultation.js
+++ frontend/src/components/Consultation.js
@@ -1,9 +1,60 @@
 import React from "react";
-import {Button, Col, Container, Row} from "react-bootstrap";
+import {Button, Col, Container, FormControl, Row} from "react-bootstrap";
 
 export default class Consultation extends React.Component {
     constructor(props) {
         super(props);
+        this.state = {
+            report : "",
+            medication : { name :  "Choose medication ..." },
+            dateStartTherapy : {},
+            dateEndTherapy : {},
+            medications : [{
+                Id: 0,
+                name: "aspirin",
+                type: "antibiotic",
+                dose: 20,
+                medicationShape: "capsule",
+                manufacturer: "hemofarm",
+                medicationIssue: "withPrescription",
+                note: "Be careful",
+                ingredient: [{
+                    Id: 0,
+                    name: "paracetamol"
+                },
+                    {
+                        Id: 1,
+                        name: "pera"
+                    }],
+                sideEffect: [{
+                    Id: 0,
+                    name: "osip"
+                }]
+            },
+                {
+                Id : 0,
+                name : "BS",
+                type : "probotic",
+                dose : 20,
+                medicationShape : "capsule",
+                manufacturer : "hemofarm",
+                medicationIssue : "withPrescription",
+                note : "Be careful",
+                ingredient :[ {
+                    Id : 0,
+                    name : "paracetamol"
+                },
+                    {
+                        Id : 1,
+                        name : "pera"
+                    }],
+                sideEffect : [{
+                    Id : 0,
+                    name : "osip"
+                }]
+
+            }]
+        }
     }
 
     render() {
@@ -11,15 +62,47 @@ export default class Consultation extends React.Component {
           <Container>
               <br/>
               {this.getPatientsInfo()}
+              <br/>
+              <label  style={{fontSize : 20, marginLeft : 55, textDecoration : "underline"}}> Report </label>
+              <textarea className="form-control"  rows="5" cols="10" placeholder={"Type report..."} name="report" onChange={this.handleInputChange} value={this.state.report} ></textarea>
+              <br/>
+              <label style={{fontSize : 20, marginLeft : 55, textDecoration : "underline"}}> Choose medication </label>
+
+              {this.drugs()}
               <Button onClick={() => this.finishAppointment()}> Finish </Button>
           </Container>
         );
     }
 
+    drugs = () => {
+        const Drugs = this.state.medications.map((medication, index) =>
+             <option value={medication.name} key={index}> {medication.name} </option>
+        );
+        return(
+            <div>
+                <select value={this.state.medication.name}  onChange={this.chooseMedication}>
+                    <option disabled> Choose medication ...</option>
+                    {Drugs}
+                </select>
+            </div>
+        )
+    }
+    chooseMedication = (event) => {
+        this.setState({
+            medication: this.state.medications.filter(m => m.name === event.target.value)[0]
+        })
+    }
+    handleInputChange = (event) => {
+        const target = event.target;
+        this.setState({
+            [target.name] : target.value
+        })
+    }
+
     getPatientsInfo = () => {
         return (
             <div>
-                <label style={{fontSize : 20, marginLeft : 55}} > Information about patient :  </label>
+                <label style={{fontSize : 20, marginLeft : 55, textDecoration : "underline"}} > Information about patient :  </label>
                 <Row>
                     <label style={{fontSize : 20}} className="col-sm-2 col-form-label">  <b> First name: </b></label>
                     <label style={{fontSize : 20}} className="col-sm-2 col-form-label"> {this.props.appointment.patient.firstName} </label>
