eb0a8c0ed7c32beca77b07ef364a55573a44bbda
Ilija Brdar <ilija_brdar@yahoo.com>
2021-01-18 23:17:30 +0100

    #1 fetch methods for lazy loaded ingredients, side effects and alternative medications

--- backend/src/main/java/app/controller/impl/MedicationControllerImpl.java
+++ backend/src/main/java/app/controller/impl/MedicationControllerImpl.java
@@ -1,6 +1,8 @@
 package app.controller.impl;
 
+import app.model.medication.Ingredient;
 import app.model.medication.Medication;
+import app.model.medication.SideEffect;
 import app.service.MedicationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -44,9 +46,24 @@ public class MedicationControllerImpl {
 
     @DeleteMapping(value = "/{id}")
     public ResponseEntity<Void> delete(@PathVariable Long id) {
-            if(!medicationService.existsById(id))
-                return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-            medicationService.delete(id);
-            return new ResponseEntity<>(HttpStatus.OK);
+        if(!medicationService.existsById(id))
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        medicationService.delete(id);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @GetMapping(value = "/ingredients/{id}")
+    public ResponseEntity<Collection<Ingredient>> getMedicationIngredients(Long id) {
+        return new ResponseEntity<>(medicationService.fetchMedicationIngredients(id), HttpStatus.OK);
+    }
+
+    @GetMapping(value = "/sideeffects/{id}")
+    public ResponseEntity<Collection<SideEffect>> getMedicationSideEffects(Long id) {
+        return new ResponseEntity<>(medicationService.fetchMedicationSideEffects(id), HttpStatus.OK);
+    }
+
+    @GetMapping(value = "/alternatives/{id}")
+    public ResponseEntity<Collection<Medication>> getMedicationAlternatives(Long id) {
+        return new ResponseEntity<>(medicationService.fetchMedicationAlternatives(id), HttpStatus.OK);
     }
 }
--- backend/src/main/java/app/service/MedicationService.java
+++ backend/src/main/java/app/service/MedicationService.java
@@ -1,7 +1,13 @@
 package app.service;
 
+import app.model.medication.Ingredient;
 import app.model.medication.Medication;
+import app.model.medication.SideEffect;
+
+import java.util.Collection;
 
 public interface MedicationService extends CRUDService<Medication> {
-    // methods for fetching lazy loaded entities
+    Collection<Ingredient> fetchMedicationIngredients(Long id);
+    Collection<SideEffect> fetchMedicationSideEffects(Long id);
+    Collection<Medication> fetchMedicationAlternatives(Long id);
 }
