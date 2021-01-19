5ae8d37efa43dec8f527df15532cf427a0de11f5
Ilija Brdar <ilija_brdar@yahoo.com>
2021-01-19 02:00:40 +0100

    #39 pharmacy search on backend

---
+++ backend/src/main/java/app/dto/PharmacySearchDTO.java
@@ -0,0 +1,43 @@
+package app.dto;
+
+public class PharmacySearchDTO {
+    private String name;
+    private String street;
+    private String town;
+    private String country;
+
+    public PharmacySearchDTO() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getTown() {
+        return town;
+    }
+
+    public void setTown(String town) {
+        this.town = town;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+}
