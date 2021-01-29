a29fbdfcf293838ea34a160203078c9f35f043b3
Tamara Kovacevic <t.kovacevic98@gmail.com>
2021-01-29 22:48:48 +0100

    delete EmailController interface

--- backend/src/main/java/app/controller/EmailController.java
+++
@@ -1,7 +0,0 @@
-package app.controller;
-
-public interface EmailController {
-    public void sendMail(String to, String subject, String body);
-    public void sendMaiConfirmation(String to, String subject, String body, String link);
-
-}
