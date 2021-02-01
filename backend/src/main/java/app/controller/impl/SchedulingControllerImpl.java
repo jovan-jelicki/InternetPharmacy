21a5ed1bd478ce52eedeb7266bffcabc699e73d8
Ilija Brdar <ilija_brdar@yahoo.com>
2021-02-01 21:53:22 +0100

    listing dermatologist examinations

--- backend/src/main/java/app/controller/impl/SchedulingControllerImpl.java
+++ backend/src/main/java/app/controller/impl/SchedulingControllerImpl.java
@@ -6,6 +6,7 @@ import app.dto.CounselingSearchDTO;
 import app.model.appointment.Appointment;
 import app.model.user.EmployeeType;
 import app.service.CounselingService;
+import app.service.ExaminationService;
 import app.service.PharmacistService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -20,12 +21,14 @@ import java.util.stream.Collectors;
 @RequestMapping(value = "api/scheduling")
 public class SchedulingControllerImpl {
     private CounselingService counselingService;
+    private ExaminationService examinationService;
     private PharmacistService pharmacistService;
 
     @Autowired
-    public SchedulingControllerImpl(CounselingService counselingService, PharmacistService pharmacistService) {
+    public SchedulingControllerImpl(CounselingService counselingService, PharmacistService pharmacistService, ExaminationService examinationService) {
         this.counselingService = counselingService;
         this.pharmacistService = pharmacistService;
+        this.examinationService = examinationService;
     }
 
     @PostMapping(value = "/search", consumes = "application/json")
@@ -43,24 +46,35 @@ public class SchedulingControllerImpl {
     }
 
     @GetMapping(value = "/counseling-upcoming/{id}")
-    public ResponseEntity<Collection<AppointmentListingDTO>> findUpcomingByPatientId(@PathVariable Long id) {
-        Collection<AppointmentListingDTO> appointments = counselingService
-                .findUpcomingByPatientId(id)
-                .stream()
-                .map(a -> {
-                    AppointmentListingDTO listing = new AppointmentListingDTO(a);
-                    listing.setDermatologistFirstName(pharmacistService.read(a.getExaminerId()).get().getFirstName());
-                    listing.setDermatologistLastName(pharmacistService.read(a.getExaminerId()).get().getLastName());
-                    return listing;
-                })
-                .collect(Collectors.toList());
-        return new ResponseEntity<>(appointments, HttpStatus.OK);
+    public ResponseEntity<Collection<AppointmentListingDTO>> findUpcomingCounselingsByPatientId(@PathVariable Long id) {
+        return getCounselingEntity(counselingService.findUpcomingByPatientId(id), id);
+    }
+
+    @GetMapping(value = "/examination-upcoming/{id}")
+    public ResponseEntity<Collection<AppointmentListingDTO>> findUpcomingExaminationsByPatientId(@PathVariable Long id) {
+        return getCounselingEntity(examinationService.findUpcomingByPatientId(id), id);
     }
 
     @GetMapping(value = "/counseling-previous/{id}")
-    public ResponseEntity<Collection<AppointmentListingDTO>> findPreviousByPatientId(@PathVariable Long id) {
-        Collection<AppointmentListingDTO> appointments = counselingService
-                .findPreviousByPatientId(id)
+    public ResponseEntity<Collection<AppointmentListingDTO>> findPreviousCounselingsByPatientId(@PathVariable Long id) {
+        return getExaminationResponseEntity(counselingService.findPreviousByPatientId(id), id);
+    }
+
+    @GetMapping(value = "/examination-previous/{id}")
+    public ResponseEntity<Collection<AppointmentListingDTO>> findPreviousExaminationsByPatientId(@PathVariable Long id) {
+        return getExaminationResponseEntity(examinationService.findPreviousByPatientId(id), id);
+    }
+
+    private ResponseEntity<Collection<AppointmentListingDTO>> getCounselingEntity(Collection<Appointment> upcomingByPatientId, @PathVariable Long id) {
+        return getCollectionResponseEntity(upcomingByPatientId);
+    }
+
+    private ResponseEntity<Collection<AppointmentListingDTO>> getExaminationResponseEntity(Collection<Appointment> previousByPatientId, @PathVariable Long id) {
+        return getCollectionResponseEntity(previousByPatientId);
+    }
+
+    private ResponseEntity<Collection<AppointmentListingDTO>> getCollectionResponseEntity(Collection<Appointment> service) {
+        Collection<AppointmentListingDTO> appointments = service
                 .stream()
                 .map(a -> {
                     AppointmentListingDTO listing = new AppointmentListingDTO(a);
