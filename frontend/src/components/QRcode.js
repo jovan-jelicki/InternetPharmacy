ac01fdfaed1462c2435dd2cd243f5611fb53751c
Tamara <t.kovacevic98@gmail.com>
2021-02-09 01:07:36 +0100

    QR code realization

--- frontend/src/components/QRcode.js
+++ frontend/src/components/QRcode.js
@@ -1,20 +1,61 @@
 import React from 'react';
 import {Button, Container} from "react-bootstrap";
-import UnregisteredLayout from "../layout/UnregisteredLayout"
-import PharmacyListing from "../components/pharmacy/PharmacyListing"
-import MedicationListing from "../components/MedicationListing"
 import PatientLayout from "../layout/PatientLayout";
+import jsQR from "jsqr";
+import png from "png.js";
+
+const fileReader = new FileReader();
+const fileInput = document.getElementById("file");
+
+
+fileReader.onload= function(event){
+    const pngReader = new png(event.target.result);
+    pngReader.parse(function(err, pngData) {
+        if (err) throw err;
+        const pixelArray = convertPNGtoByteArray(pngData);
+        console.log(jsQR(pixelArray, pngData.width, pngData.height));
+    });
+};
+
+fileInput.onchange = function () {
+    fileReader.readAsArrayBuffer(fileInput.files[0]);
+};
+
+function convertPNGtoByteArray(pngData) {
+    console.log(pngData)
+    const data = new Uint8ClampedArray(pngData.width * pngData.height * 4);
+    for (let y = 0; y < pngData.height; y++) {
+        for (let x = 0; x < pngData.width; x++) {
+            const pixelData = pngData.getPixel(x, y);
+
+            data[(y * pngData.width + x) * 4 + 0] = pixelData[0];
+            data[(y * pngData.width + x) * 4 + 1] = pixelData[1];
+            data[(y * pngData.width + x) * 4 + 2] = pixelData[2];
+            data[(y * pngData.width + x) * 4 + 3] = pixelData[3];
+        }
+    }
+    console.log(data)
+    return data;
+}
+
+
+
+
+
+
+
 
 class QRcode extends React.Component{
     constructor(props) {
         super(props);
         this.state = {
-            image: null
+            image: null,
+            loaded:false
         };
 
         this.onImageChange = this.onImageChange.bind(this);
     }
-
+/*
     onImageChange = event => {
         if (event.target.files && event.target.files[0]) {
             let img = event.target.files[0];
@@ -23,6 +64,43 @@ class QRcode extends React.Component{
             });
         }
     };
+  */
+    onImageChange = (event)=> {
+       /* let img = event.target.files[0];
+        fileReader.readAsArrayBuffer(img);
+        this.setState({
+            image: URL.createObjectURL(img)
+        });
+        */
+
+        let img = event.target.files[0];
+        fileReader.readAsArrayBuffer(img);
+        this.setState({
+            image: URL.createObjectURL(img)
+        });
+
+
+    };
+
+    /*
+    onLoad=event=>{
+        console.log(event.target)
+
+        if (!this.state.loaded) {
+            console.log('image loaded');
+            this.setState({ loaded: true });
+        }
+        console.log("BLAA")
+        console.log(event.target)
+        const pngReader = new png(event.target);
+        pngReader.parse(function(err, pngData) {
+            if (err) throw err;
+            const pixelArray = this.convertPNGtoByteArray(pngData);
+            console.log(jsQR(pixelArray, pngData.width, pngData.height));
+        });
+    };
+
+*/
 
     render() {
         return (
@@ -32,9 +110,9 @@ class QRcode extends React.Component{
 
                     <div className="row"style={{marginTop: '1rem'}}>
                         <h>Select Image</h>
-                        <input style={{marginLeft:'1rem'}} type="file" name="myImage" onChange={this.onImageChange} />
+                        <input accept="image/png" style={{marginLeft:'1rem'}} type="file" id="file" name="myImage" onChange={this.onImageChange} />
                     </div>
-                    <img style={{height:300, width:300,marginLeft:'1rem'}} src={this.state.image} />
+                    <img style={{height:300, width:300,marginLeft:'1rem'}} src={this.state.image}  />
 
                 </Container>
             </PatientLayout>
