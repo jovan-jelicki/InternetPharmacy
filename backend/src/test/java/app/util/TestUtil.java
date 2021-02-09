c740afc2e9f991b4eefb0b848444bfc86c7eef4d
jovan-jelicki <jovanzte@gmail.com>
2021-02-10 00:21:11 +0100

    TestUtil

--- backend/src/test/java/app/util/TestUtil.java
+++ backend/src/test/java/app/util/TestUtil.java
@@ -6,7 +6,7 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class TestUtil {
-    
+
     public static String json(Object object) throws IOException {
         ObjectMapper mapper = new ObjectMapper();
         mapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
