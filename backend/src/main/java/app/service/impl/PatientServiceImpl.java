9f3e2b1d7fc911f8a02203b411ccece89bfa5514
Ilija Brdar <ilija_brdar@yahoo.com>
2021-02-11 01:10:04 +0100

    bug fix

--- backend/src/main/java/app/service/impl/PatientServiceImpl.java
+++ backend/src/main/java/app/service/impl/PatientServiceImpl.java
@@ -115,6 +115,7 @@ public class PatientServiceImpl implements PatientService {
                 save(p);
             }
         });
+    }
 
     @Override
     public Boolean setPatientCategory(Long patientId) {
