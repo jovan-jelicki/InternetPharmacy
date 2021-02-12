c1f14e06b69ccf7735454375218557722caec9ee
Tamara <t.kovacevic98@gmail.com>
2021-02-12 12:55:52 +0100

    login test bug fixes

--- backend/src/test/java/app/service/LogInTest.java
+++ backend/src/test/java/app/service/LogInTest.java
@@ -3,10 +3,12 @@ package app.service;
 import app.dto.LoginDTO;
 import app.dto.LoginReturnDTO;
 import app.model.user.*;
-import app.service.impl.FilterUserDetailsService;
+import app.repository.*;
+import app.service.impl.*;
 import org.checkerframework.checker.units.qual.C;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -24,164 +26,114 @@ import static org.mockito.Mockito.when;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class LogInTest {
-
     @Mock
+    PatientRepository patientRepository;
+    @Mock
+    SupplierRepository supplierRepository;
+    @Mock
+    PharmacistRepository pharmacistRepository;
+    @Mock
+    SystemAdminRepository systemAdminRepository;
+    @Mock
+    PharmacyAdminRepository pharmacyAdminRepository;
+    @Mock
+    DermatologistRepository dermatologistRepository;
+
+    @InjectMocks
+    PatientServiceImpl patientService;
+    @InjectMocks
+    PharmacistServiceImpl pharmacistService;
+    @InjectMocks
+    PharmacyAdminServiceImpl pharmacyAdminService;
+    @InjectMocks
+    DermatologistServiceImpl dermatologistService;
+    @InjectMocks
+    SystemAdminServiceImpl systemAdminService;
+    @InjectMocks
+    SupplierServiceImpl supplierService;
+    @InjectMocks
     FilterUserDetailsService filterUserDetailsService;
-    @Mock
-    PatientService patientService;
-    @Mock
-    PharmacistService pharmacistService;
-    @Mock
-    PharmacyAdminService pharmacyAdminService;
-    @Mock
-    DermatologistService dermatologistService;
-    @Mock
-    SystemAdminService systemAdminService;
-    @Mock
-    SupplierService supplierService;
-
 
     @Test
-    public void testUserLogin(){
+    public void testUserLogin() {
 
-        Credentials credentials= new Credentials();
+        Credentials credentials = new Credentials();
         credentials.setEmail("maremare@gmail.com");
         credentials.setPassword("blabla");
 
-        LoginDTO loginDTO=new LoginDTO();
+        LoginDTO loginDTO = new LoginDTO();
         loginDTO.setEmail(credentials.getEmail());
         loginDTO.setPassword(credentials.getPassword());
 
-        Credentials dermatologistCredentials= new Credentials();
+        Credentials dermatologistCredentials = new Credentials();
         dermatologistCredentials.setEmail("anamaric@gmail.com");
         dermatologistCredentials.setPassword("anita");
 
-        Credentials pharmacistCredentials= new Credentials();
+        Credentials pharmacistCredentials = new Credentials();
         pharmacistCredentials.setEmail("koepkesk@gmail.com");
         pharmacistCredentials.setPassword("fefefre");
 
-        Credentials systemAdminCredentials= new Credentials();
+        Credentials systemAdminCredentials = new Credentials();
         systemAdminCredentials.setEmail("dsdsdsdsk@gmail.com");
         systemAdminCredentials.setPassword("fefefre");
 
-        Credentials supplierCredentials= new Credentials();
+        Credentials supplierCredentials = new Credentials();
         supplierCredentials.setEmail("errew@gmail.com");
         supplierCredentials.setPassword("fefefre");
 
-        Credentials pharmacyAdminCredentials= new Credentials();
+        Credentials pharmacyAdminCredentials = new Credentials();
         pharmacyAdminCredentials.setEmail("ssssssss@gmail.com");
         pharmacyAdminCredentials.setPassword("fefefre");
 
-        Patient patient=new Patient();
+        Patient patient = new Patient();
         patient.setId(0L);
         patient.setFirstName("Marko");
         patient.setLastName("Markovic");
         patient.setCredentials(credentials);
 
-        Dermatologist dermatologist=new Dermatologist();
+        Dermatologist dermatologist = new Dermatologist();
         dermatologist.setId(0L);
         dermatologist.setFirstName("Jovana");
         dermatologist.setLastName("Anic");
         dermatologist.setCredentials(dermatologistCredentials);
 
-        Pharmacist pharmacist= new Pharmacist();
+        Pharmacist pharmacist = new Pharmacist();
         pharmacist.setId(0L);
         pharmacist.setFirstName("Marija");
         pharmacist.setLastName("Petrovic");
         pharmacist.setCredentials(pharmacistCredentials);
 
-        SystemAdmin systemAdmin= new SystemAdmin();
+        SystemAdmin systemAdmin = new SystemAdmin();
         systemAdmin.setId(0L);
         systemAdmin.setFirstName("Jelena");
         systemAdmin.setLastName("Zoric");
         systemAdmin.setCredentials(systemAdminCredentials);
 
-        Supplier supplier= new Supplier();
+        Supplier supplier = new Supplier();
         supplier.setId(0L);
         supplier.setFirstName("Pera");
         supplier.setLastName("Peric");
         supplier.setCredentials(supplierCredentials);
 
-        PharmacyAdmin pharmacyAdmin= new PharmacyAdmin();
+        PharmacyAdmin pharmacyAdmin = new PharmacyAdmin();
         pharmacyAdmin.setId(0L);
         pharmacyAdmin.setFirstName("Marko");
         pharmacyAdmin.setLastName("Petric");
         supplier.setCredentials(pharmacyAdminCredentials);
 
-        LoginReturnDTO loginReturnDTO=new LoginReturnDTO();
+        LoginReturnDTO loginReturnDTO = new LoginReturnDTO();
         loginReturnDTO.setType(UserType.ROLE_patient);
         loginReturnDTO.setId(0L);
         loginReturnDTO.setEmail("maremare@gmail.com");
 
-        when(patientService.findByEmailAndPassword(patient.getCredentials().getEmail(), patient.getCredentials().getPassword())).thenReturn(patient);
-        when(dermatologistService.findByEmailAndPassword(dermatologist.getCredentials().getEmail(), dermatologist.getCredentials().getPassword())).thenReturn(dermatologist);
-        when(pharmacistService.findByEmailAndPassword(pharmacist.getCredentials().getEmail(), pharmacist.getCredentials().getPassword())).thenReturn(pharmacist);
-        when(systemAdminService.findByEmailAndPassword(systemAdmin.getCredentials().getEmail(), systemAdmin.getCredentials().getPassword())).thenReturn(systemAdmin);
-        when(supplierService.findByEmailAndPassword(supplier.getCredentials().getEmail(), supplier.getCredentials().getPassword())).thenReturn(supplier);
-//        when(pharmacyAdminService.findByEmailAndPassword(pharmacyAdmin.getCredentials().getEmail(), pharmacyAdmin.getCredentials().getPassword())).thenReturn(pharmacyAdmin);
+        when(patientRepository.findByEmailAndPassword(patient.getCredentials().getEmail(), patient.getCredentials().getPassword())).thenReturn(patient);
+        when(dermatologistRepository.findByEmailAndPassword(dermatologist.getCredentials().getEmail(), dermatologist.getCredentials().getPassword())).thenReturn(dermatologist);
+        when(pharmacistRepository.findByEmailAndPassword(pharmacist.getCredentials().getEmail(), pharmacist.getCredentials().getPassword())).thenReturn(pharmacist);
+        when(systemAdminRepository.findByEmailAndPassword(systemAdmin.getCredentials().getEmail(), systemAdmin.getCredentials().getPassword())).thenReturn(systemAdmin);
+        when(supplierRepository.findByEmailAndPassword(supplier.getCredentials().getEmail(), supplier.getCredentials().getPassword())).thenReturn(supplier);
+        //   when(pharmacyAdminRepository.findByEmailAndPassword(pharmacyAdmin.getCredentials().getEmail(), pharmacyAdmin.getCredentials().getPassword())).thenReturn(pharmacyAdmin);
 
-        assertThat(getUserForLogIn(loginDTO), is(equalTo(loginReturnDTO)));
-    }
+        assertThat(filterUserDetailsService.getUserForLogIn(loginDTO), is(equalTo(loginReturnDTO)));
 
-    private LoginReturnDTO getUserForLogIn(LoginDTO loginDTO) {
-
-        LoginReturnDTO loginReturnDTO = new LoginReturnDTO();
-        Patient patient = this.patientService.findByEmailAndPassword(loginDTO.getEmail(), loginDTO.getPassword());
-        if (patient != null) {
-            loginReturnDTO.setId(patient.getId());
-            loginReturnDTO.setType(UserType.ROLE_patient);
-            loginReturnDTO.setEmail(patient.getCredentials().getEmail());
-            System.out.println("pacijent");
-            return loginReturnDTO;
-        }
-        Dermatologist dermatologist = this.dermatologistService.findByEmailAndPassword(loginDTO.getEmail(), loginDTO.getPassword());
-        if (dermatologist != null) {
-            loginReturnDTO.setId(dermatologist.getId());
-            loginReturnDTO.setType(dermatologist.getUserType());
-            loginReturnDTO.setEmail(dermatologist.getCredentials().getEmail());
-            System.out.println("dermatolog");
-
-            return loginReturnDTO;
-        }
-        Pharmacist pharmacist = this.pharmacistService.findByEmailAndPassword(loginDTO.getEmail(), loginDTO.getPassword());
-        if (pharmacist != null) {
-            loginReturnDTO.setId(pharmacist.getId());
-            loginReturnDTO.setType(pharmacist.getUserType());
-            loginReturnDTO.setEmail(pharmacist.getCredentials().getEmail());
-            System.out.println("pharmacist");
-
-            return loginReturnDTO;
-        }
-
-        SystemAdmin systemAdmin = this.systemAdminService.findByEmailAndPassword(loginDTO.getEmail(), loginDTO.getPassword());
-        if (systemAdmin != null) {
-            loginReturnDTO.setId(systemAdmin.getId());
-            loginReturnDTO.setType(systemAdmin.getUserType());
-            loginReturnDTO.setEmail(systemAdmin.getCredentials().getEmail());
-            System.out.println("systemAdmin");
-
-            return loginReturnDTO;
-        }
-
-        PharmacyAdmin pharmacyAdmin = this.pharmacyAdminService.findByEmailAndPassword(loginDTO.getEmail(), loginDTO.getPassword());
-        if (pharmacyAdmin != null) {
-            loginReturnDTO.setId(pharmacyAdmin.getId());
-            loginReturnDTO.setType(pharmacyAdmin.getUserType());
-            loginReturnDTO.setEmail(pharmacyAdmin.getCredentials().getEmail());
-            System.out.println("pharmacyAdmin");
-
-            return loginReturnDTO;
-        }
-
-        Supplier supplier = this.supplierService.findByEmailAndPassword(loginDTO.getEmail(), loginDTO.getPassword());
-        if (supplier != null) {
-            loginReturnDTO.setId(supplier.getId());
-            loginReturnDTO.setType(supplier.getUserType());
-            loginReturnDTO.setEmail(supplier.getCredentials().getEmail());
-            System.out.println("supplier");
-
-            return loginReturnDTO;
-        }
-        return null;
     }
 }
